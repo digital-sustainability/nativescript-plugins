d7246f7347aaba024883a25874414c968bbe154e
Noe Thalheim <noe.thalheim@inf.unibe.ch>
2021-09-16 15:24:25 +0200

    feat(app-auth) add promise callback to android

--- packages/nativescript-app-auth/index.android.ts
+++ packages/nativescript-app-auth/index.android.ts
@@ -1,5 +1,5 @@
 import { AndroidActivityResultEventData, AndroidApplication, Application, Utils } from '@nativescript/core';
-import { AuthConfiguration, ServiceConfiguration } from '.';
+import { AuthConfiguration, AuthorizeResult, ServiceConfiguration } from '.';
 import { NativescriptAppAuthCommon } from './common';
 
 // TODO: generate typing from AppAuth
@@ -9,11 +9,15 @@ const RC_AUTH = 52;
 
 export class NativescriptAppAuth extends NativescriptAppAuthCommon {
 
-  private static _onActivityResult = ({ requestCode, intent }: AndroidActivityResultEventData) => {
+  private static _onActivityResult({ requestCode, intent }: AndroidActivityResultEventData): void {
     // == null checks for undefined as well
     if (requestCode !== RC_AUTH || intent == null) {
       return
     }
+
+    // @ts-ignore
+    const {resolve, reject } = this;
+
     const response = net.openid.appauth.AuthorizationResponse.fromIntent(intent)
     const context = Utils.android.getApplicationContext();
     // Create service with default config
@@ -26,19 +30,27 @@ export class NativescriptAppAuth extends NativescriptAppAuthCommon {
       new net.openid.appauth.AuthorizationService.TokenResponseCallback ({
         onTokenRequestCompleted(resp, error) {
           if (error != null) {
-            console.error(error);
-            return 
+            reject(error);
           }
           if (resp) {
-            console.log(resp.accessToken)
+            resolve({
+              accessToken: resp.accessToken,
+              idToken: resp.idToken,
+              tokenType: resp.tokenType,
+              refreshToken: resp.refreshToken ?? '',
+              accessTokenExpirationDate: resp.accessTokenExpirationTime,
+              scopes: resp.scpoes?.split(" ") ?? [],
+              authorizationCode: resp.authorizationCode,
+            });
           }
         }
       }) 
     )
-    Application.android.off(AndroidApplication.activityResultEvent, NativescriptAppAuth._onActivityResult);
   }
 
-  private static authorizeWithConfiguration(config: ServiceConfiguration, clientId: string, redirectUri: string, scopes: string[]) {
+  private static authorizeWithConfiguration(config: ServiceConfiguration, clientId: string, redirectUri: string, scopes: string[], resolve, reject): void {
+    Application.android.once(AndroidApplication.activityResultEvent, NativescriptAppAuth._onActivityResult, {resolve, reject});
+
     const authRequestBuilder = new net.openid.appauth.AuthorizationRequest.Builder(
       config, 
       clientId, 
@@ -52,40 +64,45 @@ export class NativescriptAppAuth extends NativescriptAppAuthCommon {
     const authService = new net.openid.appauth.AuthorizationService(context)
     const authIntent = authService.getAuthorizationRequestIntent(authRequest);
 
-
     const currentActivity = (Application.android.foregroundActivity || Application.android.startActivity);
     // TODO: Add Android SDK Version check
     currentActivity.startActivityForResult(authIntent, RC_AUTH);
   }
 
-  static authorize({ serviceConfiguration, clientId, redirectUrl, issuer, scopes }: AuthConfiguration) {
-    Application.android.on(AndroidApplication.activityResultEvent, NativescriptAppAuth._onActivityResult);
+  static authorize({ serviceConfiguration, clientId, redirectUrl, issuer, scopes }: AuthConfiguration): Promise<AuthorizeResult> {
 
     if (serviceConfiguration != null) {
-        this.authorizeWithConfiguration(
-          serviceConfiguration,
-          clientId,
-          redirectUrl,
-          scopes
+        return new Promise<AuthorizeResult>((resolve, reject) => 
+          NativescriptAppAuth.authorizeWithConfiguration(
+            serviceConfiguration,
+            clientId,
+            redirectUrl,
+            scopes,
+            resolve,
+            reject
+          )
         )
     }
 
-    net.openid.appauth.AuthorizationServiceConfiguration.fetchFromIssuer(
-      android.net.Uri.parse(issuer),
-      new net.openid.appauth.AuthorizationServiceConfiguration.RetrieveConfigurationCallback({
-        onFetchConfigurationCompleted(fetchedConfiguration, error) {
-          if (error != null) {
-            console.error(error);
-            return 
+    return new Promise<AuthorizeResult>((resolve, reject) => 
+      net.openid.appauth.AuthorizationServiceConfiguration.fetchFromIssuer(
+        android.net.Uri.parse(issuer),
+        new net.openid.appauth.AuthorizationServiceConfiguration.RetrieveConfigurationCallback({
+          onFetchConfigurationCompleted(fetchedConfiguration, error) {
+            if (error != null) {
+              reject(error);
+            }
+            NativescriptAppAuth.authorizeWithConfiguration(
+              fetchedConfiguration,
+              clientId,
+              redirectUrl,
+              scopes,
+              resolve,
+              reject
+            )
           }
-          NativescriptAppAuth.authorizeWithConfiguration(
-            fetchedConfiguration,
-            clientId,
-            redirectUrl,
-            scopes
-          )
-        }
-      })
+        })
+      )
     );
   }
 }
